93232ff878b94bbda164577638741818484466f3
Amani Kiruga <amanik@dhcp-10-29-31-239.dyn.mit.edu>
2023-07-03 11:59:32 -0400

    fix minor bugs

--- src/components/HighScores.tsx
+++ src/components/HighScores.tsx
@@ -2,86 +2,68 @@ import { UserType } from "../App";
 import { ScoreType } from "../services/ScoreRepo";
 
 type HighScoresProps = {
-    userList: UserType[];
-    onGoBackToHome: () => void;
-    highScoreLevel: "easy" | "hard";
-    setHighScoreLevel: (level: "easy" | "hard") => void;
+  userList: UserType[];
+  onGoBackToHome: () => void;
+  highScoreLevel: "easy" | "hard";
+  setHighScoreLevel: (level: "easy" | "hard") => void;
 };
 
 const HighScores = (props: HighScoresProps) => {
-    return (
-        <div className="high_scores_screen">
-            <div id="high_scores_screen_heading"> High Scores</div>
+  return (
+    <div className="high_scores_screen">
+      <div id="high_scores_screen_heading"> High Scores</div>
 
-            <div className="high_scores_list">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Rank</th>
-                            <th>Username</th>
-                            <th>
-                                Score{" "}
-                                <div
-                                    className={`level easy ${
-                                        props.highScoreLevel === "easy"
-                                            ? "chosen-level"
-                                            : ""
-                                    }`}
-                                    onClick={() =>
-                                        props.setHighScoreLevel("easy")
-                                    }
-                                >
-                                    Easy
-                                </div>
-                                <div
-                                    className={`level hard ${
-                                        props.highScoreLevel === "hard"
-                                            ? "chosen-level"
-                                            : ""
-                                    }`}
-                                    onClick={() =>
-                                        props.setHighScoreLevel("hard")
-                                    }
-                                >
-                                    Hard
-                                </div>
-                            </th>
-                            <th>Last Updated</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.userList.map((el, idx) => {
-                            return (
-                                <tr key={el.uuid}>
-                                    <td>{idx + 1}.</td>
-                                    <td>{el.username}</td>
-                                    <td>
-                                        {
-                                            el[
-                                                `highscore-${props.highScoreLevel}`
-                                            ]
-                                        }
-                                    </td>
-                                    <td>
-                                        {new Date(
-                                            el.lastUpdated
-                                        ).toLocaleDateString()}
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+      <div className="high_scores_list">
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Username</th>
+              <th>
+                Score{" "}
+                <div
+                  className={`level easy ${
+                    props.highScoreLevel === "easy" ? "chosen-level" : ""
+                  }`}
+                  onClick={() => props.setHighScoreLevel("easy")}
+                >
+                  Easy
+                </div>
+                <div
+                  className={`level hard ${
+                    props.highScoreLevel === "hard" ? "chosen-level" : ""
+                  }`}
+                  onClick={() => props.setHighScoreLevel("hard")}
+                >
+                  Hard
+                </div>
+              </th>
+              <th>Last Updated</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.userList.map((el, idx) => {
+              return (
+                <tr key={el.uuid}>
+                  <td>{idx + 1}.</td>
+                  <td>{el.username || "Nameless"}</td>
+                  <td>{el[`highscore-${props.highScoreLevel}`]}</td>
+                  <td>{new Date(el.lastUpdated).toLocaleDateString()}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
 
-            <button
-                className="button high_scores_screen_btn"
-                onClick={props.onGoBackToHome}
-            >
-                Go Back to Home
-            </button>
-        </div>
-    );
+      <button
+        className="button high_scores_screen_btn"
+        onClick={props.onGoBackToHome}
+      >
+        Go Back to Home
+      </button>
+    </div>
+  );
 };
 
 export default HighScores;
